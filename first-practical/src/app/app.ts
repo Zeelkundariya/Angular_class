abf3c1a2907d4bb924e75ee8eacb535056ea7a2a
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-20 10:34:37 +0530

    feat: Add CommonModule, studentDetails array, and display list using ngFor

--- first-practical/src/app/app.ts
+++ first-practical/src/app/app.ts
@@ -1,15 +1,36 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { Login } from './login/login';
-import {Signup} from './signup/signup'
+import { Signup } from './signup/signup';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Signup, FormsModule],
+  standalone: true,
+  imports: [Login, Signup, FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  name: string = "Zeel kundariya"
+  name: string = "Zeel kundariya";
 
-}  
+  studentName = ["abc", "def", "ghi"];
+
+  studentDetails = [
+    {
+      name: "Zeel",
+      age: 19,
+      city: "Vyara"
+    },
+    {
+      name: "Rani",
+      age: 19,
+      city: "Idar"
+    },
+    {
+      name: "Devanshi",
+      age: 18,
+      city: "Bhavnagar"
+    }
+  ];
+}
